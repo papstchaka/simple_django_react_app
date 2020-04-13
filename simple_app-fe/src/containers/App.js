bd80dbc8d127abd2e4cff8126f48ba1787c23615
papstchaka <alex.christoph.ac@gmail.com>
2020-04-13 21:15:11 +0200

    added signature

--- simple_app-fe/src/containers/App.js
+++ simple_app-fe/src/containers/App.js
@@ -5,9 +5,12 @@ import '../style/main.scss';
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <Home />
-      </Fragment>
+      <div>
+        <Fragment>
+          <Home />
+        </Fragment>
+        <a href="https://github.com/papstchaka" target="_blank" style={{position: "fixed", color: "black", bottom: "5px", right: "5px"}}>[2020] Alexander Christoph</a>
+      </div>
     );
   }
 }
